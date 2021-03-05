2df7dc1ab8e25f511fbd5153e9f59019152e8bbb
Emmanuel Ojeah <ojeahemmanuel@outlook.com>
2021-03-05 00:15:41 +0000

    add padding for time less than 10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     // var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
     // let year = new Date().getFullYear();
-    const difference = +new Date('April 12, 2021 23:00:00') - +new Date();
+    const difference = +new Date('April 12, 2021 06:00:00') - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -35,13 +35,30 @@ function App() {
 
   Object.keys(timeLeft).forEach(interval => {
     // console.log('timeLeft', timeLeft);
-    if (!timeLeft[interval]) {
-      return;
+    // if (!timeLeft[interval]) {
+    //   timerComponents.push(<span>00</span>);
+    //   return;
+    // }
+    if (timeLeft[interval] < 10) {
+      timeLeft[interval] = '0' + +timeLeft[interval];
     }
     if (interval !== 'seconds') {
-      timerComponents.push(<span>{timeLeft[interval]} : </span>);
+      if (!timeLeft[interval]) {
+        timerComponents.push(<span>00 {interval[0]}:</span>);
+      } else {
+        timerComponents.push(
+          <span>
+            {timeLeft[interval]} {interval[0]}:{' '}
+          </span>
+        );
+      }
     } else {
-      timerComponents.push(<span>{timeLeft[interval]}</span>);
+      if (!timeLeft[interval]) {
+        timerComponents.push(<span>00 </span>);
+      } else {
+        timerComponents.push(<span>{timeLeft[interval]}</span>);
+      }
+      // timerComponents.push(<span>{timeLeft[interval]}</span>);
     }
     // console.log(timerComponents);
   });
